b0c571d369d0f22ae35619acdb7df82ce983f33b
Ryan Tam <ryantam626@gmail.com>
2024-02-04 19:26:01 +0000

    feat: Add script to open up all gnome-shell-extension for ease of install

---
+++ gnome-shell-extensions.sh
@@ -0,0 +1,8 @@
+xdg-open https://extensions.gnome.org/extension/1460/vitals/
+xdg-open https://extensions.gnome.org/extension/4290/disable-workspace-switch-animation-for-gnome-40/
+xdg-open https://extensions.gnome.org/extension/4356/top-bar-organizer/
+xdg-open https://extensions.gnome.org/extension/4412/advanced-alttab-window-switcher/
+xdg-open https://extensions.gnome.org/extension/4451/logo-menu/
+xdg-open https://extensions.gnome.org/extension/4627/focus-changer/
+xdg-open https://extensions.gnome.org/extension/5090/space-bar/
+xdg-open https://extensions.gnome.org/extension/744/hide-activities-button/
